74d2c09aeaa1672351f7e76acfd6db2158bc3b14
Felipe Kurjata <fkurjata@gmail.com>
2024-12-22 16:16:18 -0300

    iniciando consumo da API

--- src/main/java/br/com/kurjata/gerenciador_series/GerenciadorSeriesApplication.java
+++ src/main/java/br/com/kurjata/gerenciador_series/GerenciadorSeriesApplication.java
@@ -1,13 +1,21 @@
 package br.com.kurjata.gerenciador_series;
 
+import br.com.kurjata.gerenciador_series.service.ApiConsumer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GerenciadorSeriesApplication {
+public class GerenciadorSeriesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorSeriesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var apiConsumer = new ApiConsumer();
+		var json = apiConsumer.getData("https://www.omdbapi.com/?t=breaking+bad&apikey=9c920167");
+		System.out.println(json);
+	}
 }
